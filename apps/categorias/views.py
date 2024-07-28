c8344f6f657fc99830319357f3f8bb224f4c39d8
maximiliano nadalin <maximilianoenadalin@gmail.com>
2024-07-29 00:25:49 +0200

    actualizcion

--- apps/categorias/views.py
+++ apps/categorias/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status,permissions
 
-# Create your views here.
+
 
 class CategoriaView(APIView):
     permission_classes = (permission.AllowAny,)
