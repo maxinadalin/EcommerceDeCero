54cdf452e704f33a7e222a22a4259b219777ebff
maximiliano nadalin <maximilianoenadalin@gmail.com>
2024-08-08 17:19:42 +0200

    actualizaciones

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductosView,ProductosViewDetail
+from .views import ProductosView,ProductosViewDetail,ProuctoSearch,ProductoSearchCategorias
 
 urlpatterns = [
     path("productos",ProductosView.as_view()),
-    path("productosDetails/<productoId>",ProductosViewDetail.as_view())
+    path("productosDetails/<productoId>",ProductosViewDetail.as_view()),
+    path("productosSearch",ProuctoSearch.as_view()),
+    path("productosSearchCategorias",ProductoSearchCategorias.as_view())
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -4,12 +4,19 @@ from rest_framework import status, permissions
 from rest_framework.views import APIView
 from .serializers import ProductoSerializers
 from apps.productos.models import Productos
+from django.db.models import Q
+from apps.categorias.models import Categoria
 
 # Create your views here.
 
 class ProductosView(APIView):
     permission_classes = (permissions.AllowAny,)
     def get(self,request, format = None):
+        
+        sortBy = request.query_params.get("sortBy")
+        
+        if not (sortBy == "date_created" or sortBy == 'name'):
+            sortBy = "date_created"
         
         try :
              productos = Productos.objects.all()
@@ -39,3 +46,32 @@ class ProductosViewDetail(APIView):
         else :
             return Response ({"mensaje" : "no existe ningun producto con ese id"},status=status.HTTP_404_NOT_FOUND)
             
+
+class ProuctoSearch(APIView):
+    permission_classes = (permissions.AllowAny, )
+    def post(self, request, format=None):
+        data = self.request.data
+        search = data['search']
+
+        # Chequear si algo input ocurrio en la busqueda
+        if len(search) == 0:
+            # mostrar todos los productos si no hay input en la busqueda
+            search_results = Productos.objects.all()
+        else:
+            # Si hay criterio de busqueda, filtramos con dicho criterio usando Q
+            search_results = Productos.objects.filter(
+                Q(description__icontains=search) | Q(name__icontains=search)
+            )
+        search_results = ProductoSerializers(search_results, many=True)
+        return Response({'search_products': search_results.data}, status=status.HTTP_200_OK)
+
+
+class ProductoSearchCategorias(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self,request,format=None):
+        data = self.request.data
+        
+    
+        return Response({"mensaje":"probando a ver si anda esto"},status=status.HTTP_200_OK)
+        
+        
